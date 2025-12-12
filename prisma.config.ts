90aee509df68641142b141a07a9a5d86143a27d5
Acevedo <mrdanielacevedo@gmail.com>
2025-12-12 14:38:28 -0300

    chore(prisma): use DATABASE_URL only

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,6 +5,6 @@ export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: { path: "prisma/migrations" },
   datasource: {
-    url: env("DIRECT_DATABASE_URL"),
+    url: env("DATABASE_URL"),
   },
 });
